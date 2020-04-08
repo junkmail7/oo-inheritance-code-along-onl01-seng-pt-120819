fc53fb3e32302778da04c1bcd5b6da0e837c2726
William Pannell <lpann5@gmail.com>
2020-04-08 23:15:39 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,6 +1,10 @@
 require_relative "./vehicle.rb"
 class Car < Vehicle
+<<<<<<< HEAD
   def go
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
+=======
+  
+>>>>>>> 1edcc60dfc437a2747cbc58d4793a74411732103
 end
